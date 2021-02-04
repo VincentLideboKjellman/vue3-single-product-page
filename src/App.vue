7711ac04f4d6f5f1bb36baa563251a62fec5ea27
Vincent Kjellman <vincent.kjellman@changetheagency.se>
2021-02-04 11:12:21 +0100

    Add webm

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Socks/>
+  <div class="background-div">
+  <div class="viedo-div">
+    <video controls width="500">
+      <source src="battery.webm"
+              type="video/webm">
+      <!-- <source src=".mp4"
+              type="video/mp4"> -->
+    </video>
+  </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Socks from './components/Socks.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Socks
   }
 }
 </script>
 
 <style>
+.video-div{
+  position: absolute;
+  top: 20%;
+}
+.background-div{
+  width:100%;
+  height:100vh;
+  position:relative;
+  background-color: green;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
